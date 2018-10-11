39bd23e7e01de2e2f4daf9e70a50bc8cd2d31ecf
Johannes Filter <hi@jfilter.de>
2018-10-11 21:45:54 +0200

    add domain to allowed domain

--- german_media_rss/settings.py
+++ german_media_rss/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "override me")
 DEBUG = True if os.getenv("NODEBUG") is None else False
 
 # TODO: Change your domain names here.
-ALLOWED_HOSTS = ["web", "localhost"] if os.getenv("NODEBUG") is None else [".yourdomain.com"]
+ALLOWED_HOSTS = ["web", "localhost"] if os.getenv("NODEBUG") is None else [".rss-suche.vis.one"]
 
 # Application definition
 
