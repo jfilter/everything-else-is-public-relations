45bb0c0c595333ad7bbe0f1b6941547426c9a380
Johannes Filter <hi@jfilter.de>
2018-10-25 20:21:39 +0200

    use `get_retries` for retries with Requests

--- main/util.py
+++ main/util.py
@@ -3,7 +3,7 @@ import logging
 import time
 from urllib import parse
 
-import requests
+import get_retries
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -11,22 +11,9 @@ logger = logging.getLogger(__name__)
 # horrible code
 
 
-def fetch(url, max_tries=5):
+def fetch(url):
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36"}
-    cur_try = 0
-    while cur_try < max_tries:
-        try:
-            response = requests.get(url, headers=headers, timeout=30)
-            # for 4xx, return instantly because another requests does not make sense
-            if 400 <= response.status_code < 500:
-                return None
-            if 500 <= response.status_code < 600:
-                response.raise_for_status()
-            return response
-        except:
-            cur_try += 1
-            time.sleep(cur_try * 10)  # increase sleep
-    return None
+    return get_retries.get(url, headers=headers)
 
 
 def create_abs_urls(urls, base_url):
